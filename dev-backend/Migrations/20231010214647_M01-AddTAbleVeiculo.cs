af407727bab536d5732b8916eed5b746d97fed60
Gabriel Laignier <gabriellaignier15@gmail.com>
2023-10-10 18:48:18 -0300

    Banco de Dados

---
+++ dev-backend/Migrations/20231010214647_M01-AddTAbleVeiculo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dev_backend.Migrations
+{
+    public partial class M01AddTAbleVeiculo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Placa = table.Column<int>(type: "int", nullable: false),
+                    AnoFabricação = table.Column<int>(type: "int", nullable: false),
+                    AnoModelo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculo");
+        }
+    }
+}
---
+++ dev-backend/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dev_backend.Models
+{
+    [Table("Veiculo")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar a placa!")]
+        public int Placa { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o Ano de Fabricação!")]
+        public int AnoFabricação { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o Ano do Modelo!")]
+        public int AnoModelo { get; set; }
+    }
+}
